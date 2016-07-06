a10f1124ff89a8542ee2cfea18bd90c0959b3ea9
AlenkinOleg <oleg7121994@mail.ru>
2016-07-06 20:33:07 +0300

    correction

--- code/retina.py
+++ code/retina.py
@@ -8,6 +8,9 @@ import theano.tensor as T
 
 
 def cross(vec1, vec2):
+    """
+    Cross product.
+    """
     
     return T.as_tensor([
         vec1[1]*vec2[2] - vec1[2]*vec2[1],
@@ -21,6 +24,7 @@ a = T.vector("a", dtype='float64')
 b0 = T.vector("b0", dtype='float64')
 b = T.vector("b", dtype='float64')
 
+#theano realization of distance function: ||(m,[u,v])|| / ||[u,v]||, where u, v - directing vectors, m connects any points of these lines
 dist = T.sqrt(T.sum((a*cross((a0-b0), b))**2))/T.sqrt(T.sum(cross(a,b)**2))
 
 distance = theano.function([a0, a, b0, b], dist)
@@ -28,6 +32,13 @@ distance = theano.function([a0, a, b0, b], dist)
 def distance_between_skew_lines(start1, direction1, start2, direction2):
     """
     Find distance between 2 lines. Every line should be presented by 2 vectors: starting point, directing vector.
+    Input:
+        start1 - vector of any point on the first line;
+        direction1 - directing vector of the first line;
+        start2 - vector of any point on the second line;
+        direction2 - directing vector of the second line.
+    Output:
+        distance - distance between lines.
     """
     
     return distance(start1, direction1, start2, direction2)
@@ -40,18 +51,35 @@ track0 = T.vector("track0", dtype='float64')
 track = T.vector("track", dtype='float64')
 sig = T.scalar("sig", dtype="float64")
 
+#theano realization of retina_response function: sum(exp(-dist**2/sigma**2))), where dist() is distance_between_skew_lines()
 rs, updates = theano.scan(fn = lambda point, direction, tr0, tr, s:
-                          T.exp(-(T.sqrt(T.sum((direction*cross((point-tr0), tr))**2))/T.sqrt(T.sum(cross(direction,tr))**2))**2/s**2),
+                          T.exp(-(T.sqrt(T.sum((direction*cross((point-tr0), tr))**2))/T.sqrt(T.sum(cross(direction,tr)**2)))**2/s**2),
                           sequences=[points, directions],
                           non_sequences=[track0, track, sig])
     
 r = rs.sum()
 R = theano.function([track0, track, points, directions, sig], r)
 
-def artifitial_retina_response(track_start, track_direction, tubes_starts, tubes_directions, sigma):
+def artifitial_retina_response(track_start, track_direction, coordinates, sigma):
     """
     Find retina response for a particular track and set of tubes.
+    Input:
+        track_start - vector of any point of track;
+        track_direction - directing vector of track;
+        coordinates - matrix containing coordinates of ends of tubes, one row = ['Wx1', 'Wy1', 'Wz', 'Wx2', 'Wy2', 'Wz'];
+        sigma - constant.
+    Output:
+        R - retina response for event.
     """
+    
+    tubes_starts = []
+    tubes_directions = []
+    for i in range(len(coordinates)):
+        a0, a = points2vec(coordinates[i])
+        tubes_starts.append(a0)
+        tubes_directions.append(a)
+    tubes_starts = np.array(tubes_starts)
+    tubes_directions = np.array(tubes_directions)
     
     return R(track_start, track_direction, tubes_starts, tubes_directions, sigma)
 
@@ -74,31 +102,23 @@ def params2vec(l, x0, m, y0):
     a = np.array([x2-x1, y2-y1, z2-z1])
     return a0, a
 
-def plot_artifitial_retina_response(event, x0s, ls, y0s, ms):
+def plot_artifitial_retina_response(grid, ms, y0s, ls, x0s):
     """
-    Take 4d-grid and return projections on 2 planes.
+    Create projections on XZ and YZ.
+    (x-x0)/l=(y-y0)/m=z (*)
+    Input:
+        grid - 4d-grid with retina responses for different combinations of parameters of track;
+        x0s, ls, yos, ms - arrays with values of parameters from parametric equation(*) of line.
+    Output:
+        fig1 - projection of artifitial_retina_response on XZ;
+        fig2 - projection of artifitial_retina_response on YZ.
     """
     
-    A0 = []
-    A = []
-    for i in range(len(event.index)):
-        a0, a = points2vec(event[['Wx1', 'Wy1', 'Wz', 'Wx2', 'Wy2', 'Wz']].values[i])
-        A0.append(a0)
-        A.append(a)
-    A0 = np.array(A0)
-    A = np.array(A)
-    
-    grid = np.ndarray(shape=(len(x0s), len(ls), len(y0s), len(ms)))
-    for i in range(len(x0s)):
-        for j in range(len(ls)):
-            for s in range(len(y0s)):
-                for t in range(len(ms)):
-                    track_point, track_direction = params2vec(ls[j], x0s[i], ms[t], y0s[s])
-                    grid[i, j, s, t] = artifitial_retina_response(track_point, track_direction, A0, A, 5)
-    
+    #initialization of projection matrixes
     projection_on_yz = np.zeros((grid.shape[3], grid.shape[2]), dtype="float64")
     projection_on_xz = np.zeros((grid.shape[1], grid.shape[0]), dtype="float64")
     
+    #filling matrixes
     for i in range(grid.shape[1]):
         for j in range(grid.shape[0]):
             projection_on_xz[i, j] = np.max(grid[j, i, :, :])
@@ -107,6 +127,7 @@ def plot_artifitial_retina_response(event, x0s, ls, y0s, ms):
         for t in range(grid.shape[2]):
             projection_on_yz[s, t] = np.max(grid[:, :, t, s])
     
+    #creating of plt objects
     fig1 = plt.figure(figsize=(9, 7))
     plt.title("Projection on XZ", fontsize=20, fontweight='bold')
     _ = plt.imshow(projection_on_xz, aspect='auto', cmap="Blues", extent=(ls.min(), ls.max(), x0s.max(), x0s.min()))
